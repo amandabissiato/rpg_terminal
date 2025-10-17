4ded456703fede89b26b301072e1a70b2c5df323
amandabissiato <amanda.bissiato@escola.pr.gov.br>
2025-10-17 11:29:39 -0300

    up date

---
+++ app.py
@@ -0,0 +1,85 @@
+players = {
+    '1': {
+        'nome':'Nilson',
+        'idade': 24,
+        'ryos': 320.50,
+        'classe': 'Mago',
+        'familia': 'Loki',
+        'habilidades': {
+            'ataque': 10,
+            'defesa': 5,
+            'vida': 10,
+        },
+        'nivel': 100,
+        'xp': 0,
+        'equipamentos': [2],
+        'dungeon': 1,
+
+    },
+    '2': {
+        'nome': 'Amanda',
+        'idade': 15,
+        'ryos': '1513.50',
+        'classe': 'Arqueiro',
+        'familia': 'Estrela Noturna',
+        'habilidades': {
+            'ataque': 10,
+            'defesa': 5,
+            'vida': 10,
+        },
+        'nivel': 1,
+        'xp': 0,
+        'equipamentos': [0],
+        'dungeon': 1,}}
+
+        equipamentos = {
+    '1': {
+       'nome': 'Espada de Madeira',
+       'classe': 'Guerreiro',
+       'habilidades': {
+           'ataque': 1,
+           'defesa': 1,
+           'vida': 0,}
+    },
+'2': {
+        'nome': 'Livro Vermelho',
+        'classe': 'Mago',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 1,
+            'defesa': 0,
+            'vida': 1,}
+    },
+    '3':{
+        'nome': 'Arco de Madeira',
+        'classe': 'Arqueiro',
+        'nivel': 1,
+        'habilidades': {
+            'ataque': 2,
+            'defesa': 0,
+            'vida': 0,}
+    },
+    }
+
+    monstros = {
+        1: {'nome': 'Rato',
+            'habilidades': {
+                'ataque': 1,
+                'defesa': 0,
+                'vida': 1,}
+           },
+        2: {'nome': 'Formiga',
+            'habilidades': {
+                'ataque': 1,
+                'defesa': 0,
+                'vida': 1,}
+
+        },
+        3: {'nome': 'Coelho',
+            'habilidades': {
+                'ataque': 4,
+                'defesa': 3,
+                'vida': 1,}
+        },
+    }
+
